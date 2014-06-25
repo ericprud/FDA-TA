4ffd2479a06c7161c35ed944e0625129da8e1ddf
ericprud <eric@w3.org>
2014-06-25 07:14:36 -0400

    ~ rearrange rules

--- tests/RenalTransplantation/infer.sh
+++ tests/RenalTransplantation/infer.sh
@@ -1,11 +1,10 @@
 
 
 for RULE in \
+ 'INSERT { ?SUB rdfs:subClassOf ?SUPER } WHERE { ?SUPER owl:disjointUnionOf ?l . ?l rdf:rest*/rdf:first ?SUB }' \
  'INSERT { ?x a ?c } WHERE { ?c owl:equivalentClass [ owl:onProperty ?p ; owl:hasValue ?v ] . ?x ?p ?v }' \
  'INSERT { ?I ?P ?V } WHERE { ?I a ?C . ?C rdfs:subClassOf [ owl:onProperty ?P ; owl:hasValue ?V ] }' \
- 'INSERT { ?S ?P2 ?O} WHERE { ?S ?P1 ?O . ?P1 rdfs:subPropertyOf ?P2 }' \
  'INSERT { ?CHILD rdfs:subClassOf ?PARENT } WHERE { ?CHILD rdfs:subClassOf/rdfs:subClassOf+ ?PARENT }' \
- 'INSERT { ?CHILD a ?CLASS } WHERE { ?CHILD rdf:type/rdfs:subClassOf ?CLASS }' \
  'INSERT { ?SUBJECT a ?CLASS } WHERE { ?SUBJECT ?p ?o . ?p rdfs:domain ?CLASS }' \
  'INSERT { ?OBJECT a ?CLASS } WHERE { ?s ?p ?OBJECT . ?p rdfs:range ?CLASS FILTER (!isLiteral(?OBJECT)) }' \
  'INSERT {?s a ?c} WHERE {       # BPAR really does require reasoning.
@@ -20,6 +19,8 @@ for RULE in \
  'INSERT {?o a ?c2} WHERE {
      ?c1 rdfs:subClassOf [ owl:onProperty ?p ; owl:allValuesFrom ?c2 ] .
      ?s a ?c1 ; ?p ?o }' \
+ 'INSERT { ?CHILD a ?CLASS } WHERE { ?CHILD rdf:type/rdfs:subClassOf ?CLASS }' \
+ 'INSERT { ?S ?P2 ?O} WHERE { ?S ?P1 ?O . ?P1 rdfs:subPropertyOf ?P2 }' \
 ; do sparql -i $1 -e "\
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
