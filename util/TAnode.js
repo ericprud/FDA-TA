40547640820a0387505a69feb20dee3f272ce0c4
ericprud <eric@w3.org>
2014-08-06 05:14:45 -0400

    ~ make spreadsheet parser asynchronous

--- util/TAnode.js
+++ util/TAnode.js
@@ -15,32 +15,55 @@ var ta = taParser.parse(taFile); // console.warn(ta);
 var turtlify = require("./TAprocessor").toTurtle;
 var defined = [];
 
-var defns = {};
+var spreadsheetParser = null;
 
-{
-    var defsFile = FS.readFileSync(PATH.normalize(process.argv[3]), "utf8");
-    var lines = defsFile.split("\n");
-    var headings = lines.shift().split('\t');
-    for (var i=0; i<lines.length; i++) {
-        if (lines[i].length == 0)
-            break;
-        var vals = lines[i].split('\t');
-        var ob = {line:i+1, file:process.argv[3]};
-        // if (vals[0] == 'dressing & grooming x - dress')
-        //     console.warn("HERE!");
-        for (h in headings)
-            if (vals[h] != undefined && vals[h] != '')
-                ob[headings[h]] = vals[h].replace(/\\n/g , "\n");
-        defns[ob['Concept Name']] = ob;
-    }
+if (true) {
+    // CSV parser
+
+    spreadsheetParser = {
+        parse: function (func) {
+            var defsFile = FS.readFileSync(PATH.normalize(process.argv[3]), "utf8");
+            var lines = defsFile.split("\n");
+            var headings = lines.shift().split('\t');
+            var defns = {};
+            for (var i=0; i<lines.length; i++) {
+                if (lines[i].length == 0)
+                    break;
+                var vals = lines[i].split('\t');
+                var ob = {line:i+1, file:process.argv[3]};
+                // if (vals[0] == 'dressing & grooming x - dress')
+                //     console.warn("HERE!");
+                for (h in headings)
+                    if (vals[h] != undefined && vals[h] != '')
+                        ob[headings[h]] = vals[h].replace(/\\n/g , "\n");
+                defns[ob['Concept Name']] = ob;
+            }
+            func(defns);
+        }
+    };
+} else {
+    // excel parser for e.g. 'RenalTransplantation-definitions.xslx'
+
+    spreadsheetParser = {
+        parse: function (func) {
+            var parseXlsx = require('excel');
+            parseXlsx(PATH.normalize(process.argv[3]), function(err, data) {
+                if(err) throw err;
+                // data is an array of arrays
+                var defns = {};
+                func(defns);
+            });
+        }
+    };
 }
 
-console.log(turtlify(ta, taParser.yy.name, taParser.yy.type, taParser.yy.imports, allEndpoints, defined, defns,
-                     function () { console.warn.apply(null, arguments); },
-                     function (file, line, column) {
-                         var ret = file + ":" + line;
-                         if (column !== undefined)
-                             ret += ":"+(column+1);
-                         ret += ": error: ";
-                         return ret;
-                     }));
+spreadsheetParser.parse(function (defns) {
+    console.log(turtlify(ta, taParser.yy.name, taParser.yy.type, taParser.yy.imports, allEndpoints, defined, defns,
+                         function () { console.warn.apply(null, arguments); },
+                         function (file, line, column) {
+                             var ret = file + ":" + line;
+                             if (column !== undefined)
+                                 ret += ":"+(column+1);
+                             ret += ": warning: ";
+                             return ret;
+                         }))});
